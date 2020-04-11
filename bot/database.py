5393e8376794f6f1946385aa82c1d5fd10a6d238
koumakpet <koumakpet@protonmail.com>
2020-04-11 19:28:27 +0200

    Database handling

---
+++ bot/database.py
@@ -0,0 +1,37 @@
+
+import sqlite3 as lite
+import logging
+from bot.constants import Database
+
+log = logging.getLogger(__name__)
+
+
+class SQLite():
+    def __init__(self):
+        self.conn = lite.connect(Database.db_name)
+        self.cur = self.conn.cursor()
+
+    def close(self):
+        self.conn.close()
+
+    def execute(self, *args):
+        self.cur.execute(*args)
+        self.conn.commit()
+
+    def create_init_tables(self):
+        try:
+            self.execute('''CREATE TABLE infractions(
+                            UID INTEGER,
+                            Type TEXT,
+                            Reason TEXT,
+                            Start TEXT,
+                            Duration INTEGER
+                            );''')
+            self.execute('''CREATE TABLE users(
+                            UID INTEGER,
+                            Muted INTEGER,
+                            Banned INTEGER
+                            );''')
+            log.info('Database tables created')
+        except lite.OperationalError:
+            log.debug('Tables exists')
