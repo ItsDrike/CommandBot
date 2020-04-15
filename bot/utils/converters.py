421d3b46315581ce025e3310ac4ca4ae3fd729b3
koumakpet <koumakpet@protonmail.com>
2020-04-15 12:12:56 +0200

    Moved Duration converter from time to converters

--- bot/utils/converters.py
+++ bot/utils/converters.py
@@ -1,7 +1,11 @@
-import discord
+import datetime
 import logging
-from discord.ext.commands import Context, UserConverter, BadArgument
+import re
 import typing as t
+
+import discord
+from dateutil.relativedelta import relativedelta
+from discord.ext.commands import BadArgument, Context, Converter, UserConverter
 
 log = logging.getLogger(__name__)
 
@@ -29,6 +33,50 @@ def proxy_user(user_id: str) -> discord.Object:
     user.bot = False
 
     return user
+
+
+class Duration(Converter):
+    """Convert duration strings into UTC datetime.datetime objects."""
+
+    duration_parser = re.compile(
+        r"((?P<years>\d+?) ?(years|year|Y|y) ?)?"
+        r"((?P<months>\d+?) ?(months|month|m) ?)?"
+        r"((?P<weeks>\d+?) ?(weeks|week|W|w) ?)?"
+        r"((?P<days>\d+?) ?(days|day|D|d) ?)?"
+        r"((?P<hours>\d+?) ?(hours|hour|H|h) ?)?"
+        r"((?P<minutes>\d+?) ?(minutes|minute|M) ?)?"
+        r"((?P<seconds>\d+?) ?(seconds|second|S|s))?"
+    )
+
+    @classmethod
+    def convert(self, duration: str) -> datetime:
+        """
+        Converts a `duration` string to a datetime object that's `duration` in the future.
+
+        The converter supports the following symbols for each unit of time:
+        - years: `Y`, `y`, `year`, `years`
+        - months: `m`, `month`, `months`
+        - weeks: `w`, `W`, `week`, `weeks`
+        - days: `d`, `D`, `day`, `days`
+        - hours: `H`, `h`, `hour`, `hours`
+        - minutes: `M`, `minute`, `minutes`
+        - seconds: `S`, `s`, `second`, `seconds`
+
+        The units need to be provided in descending order of magnitude.
+        """
+        match = self.duration_parser.fullmatch(duration)
+        if not match:
+            raise BadArgument(f"`{duration}` is not a valid duration string.")
+
+        duration_dict = {unit: int(amount) for unit,
+                         amount in match.groupdict(default=0).items()}
+        delta = relativedelta(**duration_dict)
+        now = datetime.datetime.now()
+
+        end_time = now + delta
+        seconds = (end_time - now).total_seconds()
+
+        return seconds
 
 
 class FetchedUser(UserConverter):
