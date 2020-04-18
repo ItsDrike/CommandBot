785c07a419c90dc91819dfd19f821f9b718ca15c
koumakpet <koumakpet@protonmail.com>
2020-04-19 01:14:42 +0200

    Check for infraction expiry every 90 seconds

--- bot/cogs/moderation/__init__.py
+++ bot/cogs/moderation/__init__.py
@@ -1,8 +1,17 @@
 from .modlog import ModLog
 from .infractions import Infractions
+from bot.utils import infractions
+from discord.ext.tasks import loop
 
 
 def setup(bot) -> None:
     '''Load the moderation cogs.'''
     bot.add_cog(ModLog(bot))
     bot.add_cog(Infractions(bot))
+    infraction_check.start(bot)
+
+
+@loop(seconds=90)
+async def infraction_check(bot):
+    if bot.is_ready():
+        await infractions.check_infractions_expiry(bot)
