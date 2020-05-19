83284f5f8767ef976377b13f435e827e1e76af45
koumakpet <koumakpet@protonmail.com>
2020-05-19 23:43:55 +0200

    Use singular get_embed function

--- bot/cogs/chat.py
+++ bot/cogs/chat.py
@@ -1,12 +1,14 @@
-import textwrap
 from collections import defaultdict
 
-from discord import Colour, Embed, TextChannel
+from discord import Embed, TextChannel, Colour
 from discord.ext.commands import Cog, Context, command
 
 from bot import constants
 from bot.bot import Bot
+
 from bot.cogs.moderation.modlog import ModLog
+import textwrap
+
 
 prefix = constants.Bot.prefix
 
@@ -32,17 +34,17 @@ class Chat(Cog):
     async def embedbuild(self, ctx: Context) -> None:
         """Enter embed creation mode"""
         if not self.embed_mode[ctx.author]:
-            await ctx.send(f"{ctx.author.mention} You are now in embed creation mode, use `{prefix}embedhelp` for more info")
+            await ctx.send(f"{ctx.author.mention} You are now in embed creation mode, use `{prefix}help Embed` for more info")
             self.embed_mode[ctx.author] = True
             self.embed[ctx.author] = Embed()
         else:
-            await ctx.send(f":x: {ctx.author.mention} You are already in embed creation mode, use `{prefix}embedhelp` for more info")
+            await ctx.send(f":x: {ctx.author.mention} You are already in embed creation mode, use `{prefix}help Embed` for more info")
 
     @command()
     async def embedquit(self, ctx: Context) -> None:
         """Leave embed creation mode"""
         if self.embed_mode[ctx.author]:
-            await ctx.send(f"{ctx.author.mention} You are no longer in embed creation mode, any unsent embeds were cleared")
+            await ctx.send(f"{ctx.author.mention} You are no longer in embed creation mode, your embed was cleared")
             self.embed_mode[ctx.author] = False
             del self.embed[ctx.author]
         else:
@@ -51,38 +53,49 @@ class Chat(Cog):
     @command()
     async def embedshow(self, ctx: Context) -> None:
         """Take a look at the embed"""
-        try:
-            await ctx.send(embed=self.embed[ctx.author])
-        except KeyError:
-            await ctx.send(f":x: {ctx.author.mention} No active embed found, are you in embed building mode? (`{prefix}embedhelp`)")
+        embed = self.get_embed(ctx)
+
+        if not embed:
+            return
+
+        await ctx.send(embed=embed)
 
     @command()
     async def embedsend(self, ctx: Context, channel: TextChannel) -> None:
         """Send the Embed to specified channel"""
-        try:
-            channel_perms = channel.permissions_for(ctx.author)
-            if channel_perms.send_messages:
-                embed_msg = await channel.send(embed=self.embed[ctx.author])
-                await self.mod_log.send_log_message(
-                    icon_url=constants.Icons.message_edit,
-                    colour=Colour.blurple(),
-                    title="Embed message sent",
-                    thumbnail=ctx.author.avatar_url_as(static_format="png"),
-                    text=textwrap.dedent(f"""
-                        Actor: {ctx.author.mention} (`{ctx.author.id}`)
-                        Channel: {channel.mention}
-                        Message jump link: {embed_msg.jump_url}
-                    """),
-                )
-                await ctx.send(":white_check_mark: Embed sent")
-            else:
-                await ctx.send(f":x: Sorry, {ctx.author.mention} you don't have permission to send messages to this channel")
-        except KeyError:
-            await ctx.send(f":x: {ctx.author.mention} No active embed found, are you in embed building mode? (`{prefix}embedhelp`)")
+        embed = self.get_embed(ctx)
+
+        if not embed:
+            return
+
+        channel_perms = channel.permissions_for(ctx.author)
+        if channel_perms.send_messages:
+            embed_msg = await channel.send(embed=embed)
+
+            await self.mod_log.send_log_message(
+                icon_url=constants.Icons.message_edit,
+                colour=Colour.blurple(),
+                title="Embed message sent",
+                thumbnail=ctx.author.avatar_url_as(static_format="png"),
+                text=textwrap.dedent(f"""
+                    Actor: {ctx.author.mention} (`{ctx.author.id}`)
+                    Channel: {channel.mention}
+                    Message jump link: {embed_msg.jump_url}
+                """),
+            )
+            await ctx.send(":white_check_mark: Embed sent")
 
     # endregion
     # region: embed build
     # endregion
+
+    async def get_embed(self, ctx):
+        try:
+            embed = self.embed[ctx.author]
+        except KeyError:
+            await ctx.send(f":x: {ctx.author.mention} No active embed found, are you in embed building mode? (`{prefix}help Embed`)")
+            return False
+        return embed
 
 
 def setup(bot: Bot) -> None:
