ada54e8ef34f4d7fdfdebf5c164deb570be0b6fe
ItsDrike <itsdrikeofficial@gmail.com>
2020-05-24 00:48:36 +0200

    Fix help for embeds Embeds were using decorators to determine weather author is in embed mode This causes problems with the new help system - It renders the commands as unusable until the user is in embed mode - Help command tries to check, if the user can run these commands, triggering the send in decorators This will remove the decorators and use if statements inside of the commands instead

--- bot/cogs/embeds.py
+++ bot/cogs/embeds.py
@@ -11,7 +11,7 @@ from bot.cogs.moderation.modlog import ModLog
 from bot.constants import MODERATION_ROLES
 from bot.constants import Bot as BotConstant
 from bot.constants import Icons
-from bot.decorators import has_active_embed, with_role
+from bot.decorators import with_role
 from bot.utils.converters import FetchedMember
 
 prefix = BotConstant.prefix
@@ -61,16 +61,20 @@ class Embeds(Cog):
 
     @command()
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embedshow(self, ctx: Context) -> None:
         """Take a look at the embed"""
+        if not await Embeds.has_active_embed(ctx):
+            return
+
         await ctx.send(embed=embeds[ctx.author])
 
     @command()
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embedsend(self, ctx: Context, channel: TextChannel) -> None:
         """Send the Embed to specified channel"""
+        if not await Embeds.has_active_embed(ctx):
+            return
+
         channel_perms = channel.permissions_for(ctx.author)
         if channel_perms.send_messages:
             embed_msg = await channel.send(embed=embeds[ctx.author])
@@ -101,50 +105,62 @@ class Embeds(Cog):
 
     @embed_group.command(name="title")
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embed_title(self, ctx: Context, *, title: str) -> None:
         """Set embeds title"""
+        if not await Embeds.has_active_embed(ctx):
+            return
+
         embeds[ctx.author].title = title
         await ctx.send("Embeds title updated")
 
     @embed_group.command(name="description")
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embed_description(self, ctx: Context, *, description: str) -> None:
         """Set embeds title"""
+        if not await Embeds.has_active_embed(ctx):
+            return
+
         embeds[ctx.author].description = description
         await ctx.send("Embeds description updated")
 
     @embed_group.command(name="footer")
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embed_footer(self, ctx: Context, *, footer: str) -> None:
         """Set embeds footer"""
+        if not await Embeds.has_active_embed(ctx):
+            return
+
         embeds[ctx.author].set_footer(text=footer)
         await ctx.send("Embeds footer updated")
 
     @embed_group.command(name="image", aliases=["img"])
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embed_image(self, ctx: Context, *, url: str) -> None:
         """Set embeds image"""
+        if not await Embeds.has_active_embed(ctx):
+            return
+
         embeds[ctx.author].set_image(url=url)
         await ctx.send("Embeds Image URL updated")
 
     @embed_group.command(name="color", aliases=["colour"])
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embed_color(self, ctx: Context, *, color: ColourConverter) -> None:
         """Set embeds title, `color` can be HEX color or some of standard colors (red, blue, ...)"""
+        if not await Embeds.has_active_embed(ctx):
+            return
+
         embeds[ctx.author].colour = color
         await ctx.send("Embeds color updated")
 
     # region: author
     @embed_group.command(name="author", aliases=["setauthor", "authorname"])
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embed_author_name(self, ctx: Context, *, author_name: str) -> None:
         """Set authors name in embed"""
+        if not await Embeds.has_active_embed(ctx):
+            return
+
         embed = embeds[ctx.author]
         embed.set_author(
             name=author_name,
@@ -155,9 +171,11 @@ class Embeds(Cog):
 
     @embed_group.command(name="authorurl", aliases=["setauthorurl"])
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embed_author_url(self, ctx: Context, *, author_url: str) -> None:
         """Set authors URL in embed"""
+        if not await Embeds.has_active_embed(ctx):
+            return
+
         embed = embeds[ctx.author]
         embed.set_author(
             name=embed.author.name,
@@ -170,9 +188,11 @@ class Embeds(Cog):
         "setauthoricon", "authoriconurl", "setauthoriconurl"
     ])
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embed_author_icon(self, ctx: Context, *, icon_url: t.Union[FetchedMember, str]) -> None:
         """Set authors icon in embed (You can also mention user to get his avatar)"""
+        if not await Embeds.has_active_embed(ctx):
+            return
+
         embed = embeds[ctx.author]
         if type(icon_url) != str:
             icon_url = icon_url.avatar_url_as(format="png")
@@ -191,18 +211,21 @@ class Embeds(Cog):
         "fieldadd", "fieldcreate", "fieldmake", "field"
     ])
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embed_field_create(self, ctx: Context, *, title: str = "None") -> None:
         """Create new field in embed"""
+        if not await Embeds.has_active_embed(ctx):
+            return
+
         embeds[ctx.author].add_field(name=title, value="None")
         self.embed_field_id[ctx.author] += 1
         await ctx.send(f"Embed field with ID **{self.embed_field_id[ctx.author]}** created")
 
     @embed_group.command(name="fielddescription", aliases=["fieldvalue"])
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embed_field_description(self, ctx: Context, ID: int, *, description: str) -> None:
         """Set description of embeds field"""
+        if not await Embeds.has_active_embed(ctx):
+            return
         if self.embed_field_id[ctx.author] < ID or ID < 0:
             await ctx.send(f":x: {ctx.author.mention} Sorry, but there is no such field ID")
             return
@@ -217,9 +240,10 @@ class Embeds(Cog):
 
     @embed_group.command(name="fieldtitle", aliases=["fieldname"])
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embed_field_title(self, ctx: Context, ID: int, *, title: str) -> None:
         """Set title of embeds field"""
+        if not await Embeds.has_active_embed(ctx):
+            return
         if self.embed_field_id[ctx.author] < ID or ID < 0:
             await ctx.send(f":x: {ctx.author.mention} Sorry, but there is no such field ID")
             return
@@ -234,9 +258,11 @@ class Embeds(Cog):
 
     @embed_group.command(name="fieldinline")
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embed_field_inline(self, ctx: Context, ID: int, inline: bool) -> None:
         """Choose if embed should be inline or not"""
+        if not await Embeds.has_active_embed(ctx):
+            return
+
         if self.embed_field_id[ctx.author] < ID or ID < 0:
             await ctx.send(f":x: {ctx.author.mention} Sorry, but there is no such field ID")
             return
@@ -255,9 +281,11 @@ class Embeds(Cog):
         "fielddel", "delfield", "remfield"
     ])
     @with_role(*MODERATION_ROLES)
-    @has_active_embed(embeds)
     async def embed_field_remove(self, ctx: Context, ID: int) -> None:
         """Remove field in embed"""
+        if not await Embeds.has_active_embed(ctx):
+            return
+
         if self.embed_field_id[ctx.author] < ID or ID < 0:
             await ctx.send(f":x: {ctx.author.mention} Sorry, but there is no such field ID")
             return
@@ -267,6 +295,14 @@ class Embeds(Cog):
         await ctx.send(f"Embed field with ID: **{ID}** removed (all other IDs were renumbered accordingly)")
     # endregion
     # endregion
+
+    @staticmethod
+    async def has_active_embed(ctx):
+        if ctx.author in embeds:
+            return True
+        else:
+            await ctx.send(f":x: {ctx.author.mention} No active embed found, are you in embed building mode? (`{Bot.prefix}help Embeds`)")
+            return False
 
 
 def setup(bot: Bot) -> None:
